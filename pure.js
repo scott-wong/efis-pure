a3b8d0a77ad0c80dba080b4ae4826af723fb0acc
scottleo <wyuleo@gmail.com>
2014-06-05 15:27:00 +0800

    test

--- pure.js
+++ pure.js
@@ -1,6 +1,6 @@
 var fis = module.exports = require('fis');
 
-fis.cli.name = 'fis-pure';
+fis.cli.name = 'efis-pure';
 fis.cli.info = fis.util.readJSON(__dirname + '/package.json');
 
 fis.config.merge({
